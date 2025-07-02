95f1a83a1972e775eac7de9630567c00023437a5
Eeva-cyber <eevatan22@gmail.com>
2025-07-03 00:47:40 +1000

    Add AppBar and minus button

--- lib/pages/counter_page.dart
+++ lib/pages/counter_page.dart
@@ -20,35 +20,109 @@ class _CounterPageState extends State<CounterPage> {
     });
   }
 
+  void _decrementCounter () {
+    setState(() {
+      _counter--;
+    });
+  }
   // UI
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              //message
-              Text('You pushed the + button this many times:'),
-          
-              //counter value
-              Text(
-                _counter.toString(),
-                style: TextStyle(
-                  fontWeight: FontWeight.bold
-                ),
-              ),
-          
-              //button
-              ElevatedButton(
-                onPressed: _incrementCounter,
-                child: Text('+'),
-                )
-            ],
+      appBar: AppBar(
+        title: Text(
+          "Counter App",
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.white
           ),
         ),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple[400],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //message
+            Text('Push the buttons to change the counter value',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            SizedBox(height: 30),
+
+            //counter value
+            Text(
+              _counter.toString(),
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            
+            SizedBox(height: 20),
+            
+            //button
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: _incrementCounter,
+                  style: ElevatedButton.styleFrom(
+                    //button size
+                    minimumSize: Size(60, 60),
+                    //button color
+                    backgroundColor: Colors.lightBlue[200],
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  child: Text(
+                    '+',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  )
+                ),
+                
+                //space between the buttons
+                SizedBox(
+                  width: 40,
+                ),
+
+                //minus button
+                ElevatedButton(
+                  onPressed: _decrementCounter,
+                  style: ElevatedButton.styleFrom(
+                    //button size
+                    minimumSize: Size(60, 60),
+                    //button color
+                    backgroundColor: Colors.red[300],
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  child: Text(
+                    '-',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
